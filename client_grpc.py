4ea376b5e79b41708a35d4310b6e38d3f518949d
Vadim Smirnov <vdsmitnov52@gmail.com>
2020-08-10 16:01:13 +0300

    smirnov@vadim 10.10.2.40
    	изменено:      client_grpc.py

--- client_grpc.py
+++ client_grpc.py
@@ -7,10 +7,10 @@ import	grpc
 import	Api_pb2
 import	Api_pb2_grpc
 
-DEVICES = ["157231", "1284891", "1284890", "1323475"]
+DEVICES = ["157231", "1284891", "1284890", "1323475", "24964441", "353451048047755"]
 #NUMBERS = ["н756се750", "н851се750", "ат59652"]
 # Active T1
-NUMBERS = ['р984кх152', 'р999кх152', 'ар31752']
+NUMBERS = ['р984кх152', 'р999кх152', 'ар31752', 'ау20652', 'м063ов152']
 #DEVICES = []
 
 def getState(stub):
@@ -45,8 +45,8 @@ def getDataRange (stub):
 	for o in rrr.Objects:
 		print (o.StateNumber)
 		for d in o.Data:
-			print ( time.strftime("\t%d-%m-%Y %T", time.localtime(d.DeviceTime)), time.strftime("\t%d-%m-%Y %T ", time.localtime(d.ReceivedTime)))
-			print (  d.Position.Longitude, '\t', d.Position.Latitude, d.State.Mileage)
+			print (time.strftime("\t%d-%m-%Y %T", time.localtime(d.DeviceTime)), time.strftime("\t%d-%m-%Y %T ", time.localtime(d.ReceivedTime)))
+			print (d.Position.Longitude, '\t', d.Position.Latitude, d.State.Mileage)
 		print()
 
 
@@ -58,15 +58,22 @@ def getDataStream (stub):
 	#   ObjectsDataStreamRequest
 	# rrr = stub.GetObjectsEventsStream(request)    # подписка на поток событий по устройствам
 	rrr = stub.GetObjectsDataStream(request)
-	print ("\nSSS", type(request), rrr)    # help(rrr))
+	print ("\nSSS", type(request), type(rrr))    # help(rrr))
 	tt = time.time()
 	print ('running', rrr.running())     # finished with exit code 0
-	for j in range(55):
-		res = rrr.done()
+	# '''
+	for j in range(11):
+		# res = rrr.done()
+		res = rrr.debug_error_string()
+		# help(rrr)
+		# res = rrr.result(timeout=11)
 		if res:
 			print("\tresult", j, res, rrr.details())
 			break
-		# else:			time.sleep(3)
+		else:
+			print('res:', res, j)
+			time.sleep(3)
+	# '''
 	print("#"*11, int(time.time() - tt))
 	print ("details\t", rrr.details())       # Висим тихо
 	print ('add_done_callback\t', rrr.add_done_callback(callback))       # Висим тихо
@@ -95,28 +102,31 @@ def getInfo (stub):
 	# print (  rrr
 
 
-# открываем канал и создаем клиент
-# channel = grpc.insecure_channel('rnis-tm.t1-group.ru:18082')
-channel = grpc.insecure_channel('rnis-api.rnc52.ru:6161')
-stub =	Api_pb2_grpc.APIStub(channel)
-
-# help (stub.GetObjectsEventsStream)    # MultiThreadedRendezvous)
-# help(stub.GetObjectsState)
-for j in range(2):
-	print (j, "#"*22)
-	getState(stub)      # Текущее состояние
-	time.sleep(11)
-# getDataRange(stub)  # получение телематических данных за период
-getDataStream(stub)
-# getInfo(stub)
-
-''' gRPC	'р984кх152', 'р999кх152', 'ар31752'
-apis = Api_pb2_grpc.APIServicer()
-help(apis.GetObjectsState)
-help(apis.GetObjectsDataRange)
-ostate	= Api_pb2_grpc.GetObjectsState(channel)
-odrange = Api_pb2_grpc.GetObjectsDataRange(channel)
-
-print (ostate)
-print (odrange)
+def test(stub):
+	# help (stub.GetObjectsEventsStream)    # MultiThreadedRendezvous)
+	# help(stub.GetObjectsState)
+	for j in range(2):
+		print (j, "#"*22)
+		getState(stub)      # Текущее состояние
+		time.sleep(11)
+	# getDataRange(stub)  # получение телематических данных за период
+	
+	
+	''' gRPC	'р984кх152', 'р999кх152', 'ар31752'
+	apis = Api_pb2_grpc.APIServicer()
+	help(apis.GetObjectsState)
+	help(apis.GetObjectsDataRange)
+	ostate	= Api_pb2_grpc.GetObjectsState(channel)
+	odrange = Api_pb2_grpc.GetObjectsDataRange(channel)
+	
+	print (ostate)
+	print (odrange)
 '''
+if __name__ == '__main__':
+	# открываем канал и создаем клиент
+	# channel = grpc.insecure_channel('rnis-tm.t1-group.ru:18082')
+	channel = grpc.insecure_channel('rnis-api.rnc52.ru:6161')
+	stub = Api_pb2_grpc.APIStub(channel)
+	# getInfo(stub)
+	# test(stub)
+	getDataStream(stub)
